5c6eaa1a32652f2e72bbbf7419f34b708b52b09a
Matthew Bickerton <matthew@matthews-macbook-pro.local>
2018-03-04 15:17:06 -0500

    Initial Commit

---
+++ SimpleCounter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SimpleCounter
+//
+//  Created by Matthew Bickerton on 3/4/18.
+//  Copyright © 2018 Matthew Bickerton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
